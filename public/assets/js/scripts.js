c550671ca6df1076bce6c5e9ee9798e8421f516a
mandino <dino@misfit-inc.com>
2018-12-28 21:51:12 +0800

    apply logic for screen width event trigger for pin details

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -186,16 +186,18 @@ var happiness_step_1_animation = lottie.loadAnimation(happiness_step_1_params),
 		$('#' + modal_identifier).fadeIn(300);
 	});
 
-	$('.map-pin').hover(function(){
-		var hover_data = $(this).data('hover');
-		$('.map-detail.' + hover_data).addClass('active');
-	}, function() {
-		$('.map-detail').removeClass('active');
-	});
-
-	$('.map-pin').click(function () {
-		var hover_data = $(this).data('hover');
-		$('.map-detail.' + hover_data).removeClass('active').toggleClass('active');
-	});
+	if ($(window).height() > 913) {
+		$('.map-pin').hover(function () {
+			var hover_data = $(this).data('hover');
+			$('.map-detail.' + hover_data).addClass('active');
+		}, function () {
+			$('.map-detail').removeClass('active');
+		});
+	} else {
+		$('.map-pin').click(function () {
+			var hover_data = $(this).data('hover');
+			$('.map-detail.' + hover_data).removeClass('active').toggleClass('active');
+		});
+	}
 
 })(jQuery)
