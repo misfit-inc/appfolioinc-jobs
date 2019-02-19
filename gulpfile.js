86a0e9273a0d8ad4e7b53a69e9eea3e3a981c144
mandino <dino@misfit-inc.com>
2019-02-19 18:02:10 +0800

    update events content

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,12 @@ var gulp = require('gulp'),
 		sass = require('gulp-sass')
  
 gulp.task('sass', function () {
-  return gulp.src('./assets/stylesheets/*.scss')
+  return gulp.src('./assets/stylesheets/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
+    .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(gulp.dest('./public/assets/css'));
 });
  
 gulp.task('sass:watch', function () {
-	gulp.watch('./assets/stylesheets/*.scss', ['sass']);
+  gulp.watch('./assets/stylesheets/**/*.scss', gulp.series('sass'));
 });
