9990b6279498f1c8030d878e804d79e018cb768e
mandino <dino@misfit-inc.com>
2019-02-26 15:49:43 +0800

    adjust copy

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 'use strict';
  
 var gulp = require('gulp'),
-		sass = require('gulp-sass')
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps')
  
 gulp.task('sass', function () {
   return gulp.src('./assets/stylesheets/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(sourcemaps.write('./assets/css/maps'))
     .pipe(gulp.dest('./public/assets/css'));
 });
  
