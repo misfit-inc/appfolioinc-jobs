991784b7a7bc0efd4f7b2e3bf85ce4d131ad7c94
mandino <dino@misfit-inc.com>
2019-03-28 12:56:57 +0800

    add compiled CSS and JS. Also update HTML

--- gulpfile.js
+++ gulpfile.js
@@ -13,27 +13,43 @@ gulp.task('sass', function () {
     .pipe(sourcemaps.write('./assets/css/maps'))
     .pipe(gulp.dest('./public/assets/css'));
 });
+
+gulp.task('sass:concat', function () {
+	return gulp.src([
+			'./public/assets/css/motion-ui.css',
+			'./public/assets/css/owl.carousel.min.css',
+			'./public/assets/css/owl.theme.default.css',
+			'./public/assets/css/style.css',
+		])
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest('./public/assets/css'));
+});
  
 gulp.task('sass:watch', function () {
   gulp.watch('./assets/stylesheets/**/*.scss', gulp.series('sass'));
 });
 
-gulp.task('uglify', function () {
-		gulp.src([
-			'./public/assets/js/owl.carousel.js',
-			'./public/assets/js/isotope.pkgd.min.js',
-			'./public/assets/js/motion-ui.js',
-			'./public/assets/js/happiness-steps.js',
-			'./public/assets/js/why-appfolio.js',
-			'./public/assets/js/lottie.min.js',
-			'./public/assets/js/jquery.waypoints.1.min.js',
-			'./public/assets/js/jquery.waypoints.2.inview.min.js',
-			'./public/assets/js/scripts.js',
+gulp.task('concat', function () {
+		return gulp.src([
+			'./assets/js/owl.carousel.js',
+			'./assets/js/isotope.pkgd.min.js',
+			'./assets/js/motion-ui.js',
+			'./assets/js/happiness-steps.js',
+			'./assets/js/why-appfolio.js',
+			'./assets/js/lottie.min.js',
+			'./assets/js/jquery.waypoints.1.min.js',
+			'./assets/js/jquery.waypoints.2.inview.min.js',
+			'./assets/js/scripts.js',
 		])
 		.pipe(concat('main.js'))
-		.pipe(gulp.dest('./assets/js'));
-
-		return gulp.src(['./assets/js/main.js',])
-						.pipe(uglify())
-						.pipe(gulp.dest('./assets/js'));
+		.pipe(gulp.dest('./public/assets/js'));
 });
+
+gulp.task('uglify', function () {
+	return gulp.src(['./public/assets/js/main.js', ])
+		.pipe(uglify())
+		.pipe(gulp.dest('./public/assets/js'));
+});
+
+gulp.task('build-js', gulp.series('concat', 'uglify'));
+gulp.task('build-css', gulp.series('sass', 'sass:concat'));
